16c68befbb8f8279a3bcba47c329656a6892a544
sanjay <sanjusanjay8147@gmail.com>
2024-12-08 15:25:07 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 
 export default function Home() {
   return (
-  <>this is fun</>
+    <>Entha mili kutty?
+      <br/>
+      <h1>quotes by great motivator and philospher of 20th century <strong>
+         MELINA MATHEW
+      </strong></h1>
+      <br/>
+      <p>'I will develop a proper linguistic vocal cord for monkeys, lions and tigers. I will make cows and bulls talk in sanskrit and malayalam.'</p>
+        <br/>
+      <p>'There are millions of planets where life exists and they are regularly visiting the planet earth for educational purpose'</p>
+        <br/>
+      <p>'Whether you are a kid or an adult, opening your third eye will take only seconds for me'</p>
+        <br/>
+      <p>"IT IS ME, RESIDES IN THIS AS ME, IS RESIDING IN ALL OF THAT AS ME, IS TALKING TO ME "</p>
+
+    </>
   );
 }
